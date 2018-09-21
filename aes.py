8613e0b857f57936d4533baa80dcad864a6d3d3a
Chimdindu Orakwue <denalexo@cs.utexas.edu>
2018-09-21 15:56:49 -0500

    fixed sub_bytes

--- aes.py
+++ aes.py
@@ -12,16 +12,13 @@ def decrypt():
 def key_expansion():
 	return
 
-# sub_bytes not working currently
-def sub_bytes(state):
-	for block in state:
-		for column in block:
-			for i in range(len(column)):
-				val = int.from_bytes(column[i], byteorder='big', signed=True)
-				col_index = least_significant_mask & val
-				row_index = most_significant_mask & val
-				column[i] = s_box[row_index][col_index]
-	return
+def sub_bytes(block):
+	for column in block:
+		for i in range(len(column)):
+			val = int.from_bytes(column[i], byteorder='big')
+			col_index = least_significant_mask & val
+			row_index = (most_significant_mask & val) >> 4
+			column[i] = s_box[row_index][col_index].to_bytes(1, byteorder='big')
 
 def shift_rows(state):
 	return
@@ -44,7 +41,7 @@ def read_file(filename):
 				if byte != b'':
 					col.append(byte)
 				else:
-					col.append(0)
+					col.append(b'')
 				byte = file.read(1)
 			block.append(col)
 		state.append(block)
@@ -55,7 +52,9 @@ def main():
 	for i in range(len(state)):
 		print(state[i])
 		print("")
-	sub_bytes(state)
+		continue
+	print()
+	sub_bytes(state[0])
 	print(state)
 
 if __name__ == '__main__':
