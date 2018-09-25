dd0808a0345bbf1eaa078e5a8a649c5bb6eb57fe
Anjuli Goring <agoring@cs.utexas.edu>
2018-09-24 19:32:23 -0500

    added inverse shift_rows and inverse sub_bytes

--- aes.py
+++ aes.py
@@ -114,12 +114,45 @@ def key_expansion(input_key):
     return expanded_keys
 
 
+def inv_sub_bytes(block):
+    for column in block:
+        for i in range(len(column)):
+            col_index = least_significant_mask & column[i][0]
+            row_index = (most_significant_mask & column[i][0]) >> 4
+            column[i] = s_box_inv[row_index][col_index]
+
+
 def sub_bytes(block):
     for column in block:
         for i in range(len(column)):
             col_index = least_significant_mask & column[i][0]
             row_index = (most_significant_mask & column[i][0]) >> 4
             column[i] = s_box[row_index][col_index]
+
+
+def inv_shift_rows(block):
+    # row 2
+    temp = block[1][3]
+    block[1][3] = block[1][2]
+    block[1][2] = block[1][1]
+    block[1][1] = block[1][0]
+    block[1][0] = temp
+
+    # row 3
+    temp = block[2][0]
+    block[2][0] = block[2][2]
+    block[2][2] = temp
+    temp = block[2][1]
+    block[2][1] = block[2][3]
+    block[2][3] = temp
+
+    # row 4
+    temp = block[3][0]
+    block[3][0] = block[3][1]
+    temp2 = block[3][3]
+    block[3][3] = temp
+    block[3][1] = block[3][2]  
+    block[3][2] = temp2
 
 
 def shift_rows(block):
@@ -274,6 +307,11 @@ def main():
         print("Encrypted State: Block", i)
         print_2d_bytes(encrypt_128(state[i], expanded_keys))
         print("")
+
+    matrix = [[1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4]]
+    print("test inv_shift_rows")
+    inv_shift_rows(matrix)
+    print(matrix)
 
 
 if __name__ == '__main__':
